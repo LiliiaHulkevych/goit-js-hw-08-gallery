89846e53c0b90bf845e01cd659e1526c8708fafc
Liliia Hulkevych <lilit_ua@ukr.net>
2022-04-06 00:14:41 +0300

    завершена робота

--- app.js
+++ app.js
@@ -72,14 +72,18 @@ const galleryItems = [
         itemImage: document.querySelector('ul.gallery__item'),
         divModal: document.querySelector('.js-lightbox'),
         image: document.querySelector('.lightbox__image'),
-        buttonClose: document.querySelector('button[data-action="close-lightbox"]')
+        buttonClose: document.querySelector('button[data-action="close-lightbox"]'),
+        overlay: document.querySelector('.lightbox__overlay'),
+        linkImage: document.querySelector('.gallery__link')
     }
+
 
     const createListItems = createListItemsMarkup(galleryItems);
     refs.listImages.innerHTML = createListItems;
 
-    refs.listImages.addEventListener('click', openModal);
-    refs.buttonClose.addEventListener('click', closeModal)
+    refs.listImages.addEventListener('click', onOpenModal);
+    refs.buttonClose.addEventListener('click', onCloseModal);
+    refs.overlay.addEventListener('click', onOverlayClick);
   
 
     function createListItemsMarkup(items) {
@@ -100,14 +104,138 @@ const galleryItems = [
     }
 
 
-    function openModal(evt) {
+    function onOpenModal(evt) {
+        window.addEventListener('keydown', onCloseModalEsc);
+        
         refs.divModal.classList.add("is-open");
         evt.preventDefault();
+
         refs.image.src = evt.target.dataset.source;
+        refs.image.alt = evt.target.alt;
     
+        window.addEventListener('keydown', onRightKeyPress);
+        window.addEventListener('keydown', onLeftKeyPress);
     }
 
-    function closeModal(evt) {
+    function onCloseModal(evt) {
+        window.removeEventListener('keydown', onCloseModalEsc)
+        window.removeEventListener('keydown', onRightKeyPress);
+        window.removeEventListener('keydown', onLeftKeyPress);
+
         refs.divModal.classList.remove("is-open");
-        refs.image.src = ""
+        refs.image.src = "";
+        refs.image.alt = ""
     }
+
+    function onOverlayClick(evt) {
+        if(evt.currentTarget === evt.target) { 
+            onCloseModal();
+        }
+    }
+
+    function onCloseModalEsc(evt) {
+
+       if(evt.code === 'Escape') {
+        onCloseModal();
+       
+    }
+    }
+
+    function onRightKeyPress(evt) {
+        if(evt.code === 'ArrowRight') {
+
+            for (let i = 0; i < galleryItems.length; i+= 1) {
+                    if(refs.image.getAttribute('src') === galleryItems[i].original && i < galleryItems.length - 1) {
+                        refs.image.setAttribute('src', galleryItems[i + 1].original);
+                        refs.image.setAttribute('alt', galleryItems[i + 1].description);
+                        return
+                    }
+                }
+           }
+    }
+
+
+
+    function onLeftKeyPress(evt) {
+        if(evt.code === 'ArrowLeft') {
+
+            for (let i = 0; i < galleryItems.length; i+= 1) {
+                    if(refs.image.getAttribute('src') === galleryItems[i].original && i >=1) {
+                        refs.image.setAttribute('src', galleryItems[i - 1].original);
+                        refs.image.setAttribute('alt', galleryItems[i - 1].description);
+                        return
+                    }
+                }
+           }
+        }
+
+
+
+
+// СПОСІБ 2
+
+//  const arr = document.querySelectorAll('.gallery__image')
+//     const arrayConcat = Array.prototype.slice.call(arr)
+//     let mainIndex = 0
+
+//     function onOpenModal(evt) {
+//         window.addEventListener('keydown', onCloseModalEsc);
+//         refs.divModal.classList.add("is-open");
+//         evt.preventDefault();
+
+//         arrayConcat.forEach((e,i)=>{
+//             if(evt.target === e){
+//                 mainIndex = i
+//             }
+//         })
+        
+//         refs.image.src = arrayConcat[mainIndex].dataset.source;
+//         refs.image.alt = arrayConcat[mainIndex].alt;
+
+    
+//         window.addEventListener('keydown', onRightKeyPress);
+//         window.addEventListener('keydown', onLeftKeyPress);
+//     }
+
+//     function onCloseModal(evt) {
+//         window.removeEventListener('keydown', onCloseModalEsc)
+//         window.removeEventListener('keydown', onRightKeyPress);
+//         window.removeEventListener('keydown', onLeftKeyPress);
+
+//         refs.divModal.classList.remove("is-open");
+//         refs.image.src = "";
+//         refs.image.alt = ""
+//     }
+
+//     function onOverlayClick(evt) {
+//         if(evt.currentTarget === evt.target) { 
+//             onCloseModal();
+//         }
+//     }
+
+//     function onCloseModalEsc(evt) {
+
+//        if(evt.code === 'Escape') {
+//            onCloseModal();
+//         }
+//     }
+
+//     function onRightKeyPress(evt) {
+//         if(evt.code === 'ArrowRight') {
+
+//             mainIndex +=1
+//             refs.image.src = arrayConcat[mainIndex].dataset.source;
+//             refs.image.alt = arrayConcat[mainIndex].alt;
+//         }
+//     }
+
+
+
+//     function onLeftKeyPress(evt) {
+//         if(evt.code === 'ArrowLeft') {
+
+//             mainIndex -=1
+//             refs.image.src = arrayConcat[mainIndex].dataset.source;
+//             refs.image.alt = arrayConcat[mainIndex].alt;
+//         }
+//     }
